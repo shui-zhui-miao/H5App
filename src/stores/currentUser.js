d6b9096dc7b01693133a44c54256a664668b01ae
KnowSea <2069571638@qq.com>
2026-03-15 23:38:14 -0700

    5

--- src/stores/currentUser.js
+++ src/stores/currentUser.js
@@ -3,19 +3,18 @@ import currentUserData from '../data/currentUser.json'
 
 export const useCurrentUserStore = defineStore('currentUser', {
     state: () => ({
-        // 如果 window.currentUser 有值就用它，否则用默认 JSON
         currentUser: window.currentUser || currentUserData
     }),
     actions: {
         setCurrentUser(user) {
             this.currentUser = user
-            window.currentUser = user  // 保证全局 JS 对象也更新
+            window.currentUser = user
         }
     }
 })
 
-function updateCurrentUser(user) {
-    // 更新 Pinia store
+// ⭐️ 关键：挂到 window
+window.updateCurrentUser = function (user) {
     const currentUserStore = useCurrentUserStore()
     currentUserStore.setCurrentUser(user)
 }
